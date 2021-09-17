fbde360a0d126f5ce695a7998f9bf537f66c3ee3
christycrt <christycrt@gmail.com>
2021-09-18 01:16:03 +0700

    [create] searchbox

---
+++ src/components/Home/Searchbox.js
@@ -0,0 +1,11 @@
+function Searchbox() {
+  return (
+    <section className="search-section">
+      <form className="search-box">
+        <input placeholder="Search for a Meal..." />
+      </form>
+    </section>
+  );
+}
+
+export default Searchbox;
