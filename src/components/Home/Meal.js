6d9bb88b2f8466551fd9e625d8829ccbd17113f0
christycrt <christycrt@gmail.com>
2021-09-18 16:27:30 +0700

    [update] no match page

--- src/components/Home/Meal.js
+++ src/components/Home/Meal.js
@@ -4,15 +4,21 @@ function Meal({ mealList }) {
   return (
     <section id="meal">
       <h1 className="meal-title">Meal</h1>
-      <div className="row">
-        {mealList.map((item, key) => {
-          return (
-            <div key={key} className="col-4">
-              <MealCard item={item} />
-            </div>
-          );
-        })}
-      </div>
+      {mealList ? (
+        <div className="row">
+          {mealList.map((item, key) => {
+            return (
+              <div key={key} className="col-4">
+                <MealCard item={item} />
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <h2 className="no-match-text">
+          no meals matched your search criteria
+        </h2>
+      )}
     </section>
   );
 }
