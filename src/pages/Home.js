b46f48a3eee6d880eed05a23f01b3f1d85d95278
christycrt <christycrt@gmail.com>
2021-09-18 15:57:14 +0700

    [update] connect API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,33 @@
+import { useEffect, useState } from "react";
 import Navbar from "../components/common/Navbar";
 import Meal from "../components/Home/Meal";
 import Searchbox from "../components/Home/Searchbox";
 import "../styles/pages/Home.scss";
+import axios from "axios";
 
 function Home() {
+  const [mealList, setMealList] = useState([]);
+
+  useEffect(() => {
+    let fetchMeal = setTimeout(() => {
+      axios
+        .get("https://www.themealdb.com/api/json/v1/1/search.php?s=")
+        .then(function (response) {
+          // handle success
+          console.log(response.data.meals);
+          setMealList(response.data.meals);
+        });
+    }, 500);
+    return () => {
+      clearTimeout(fetchMeal);
+    };
+  }, []);
+
   return (
     <div id="home">
       <Navbar />
       <Searchbox />
-      <Meal />
+      <Meal mealList={mealList} />
     </div>
   );
 }
