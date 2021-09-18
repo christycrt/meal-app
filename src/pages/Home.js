acbcafc83d1772bfdde8628530ea15c480812b7e
christycrt <christycrt@gmail.com>
2021-09-18 18:39:32 +0700

    [create] loading page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,19 +4,25 @@ import Meal from "../components/Home/Meal";
 import Searchbox from "../components/Home/Searchbox";
 import "../styles/pages/Home.scss";
 import axios from "axios";
+import Loading from "../components/common/Loading";
 
 function Home() {
   const [mealList, setMealList] = useState([]);
   const [searchMeal, setSearchMeal] = useState("");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     let fetchMeal = setTimeout(() => {
+      setLoading(true);
       axios
-        .get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchMeal}`)
+        .get(
+          `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchMeal}`
+        )
         .then(function (response) {
           // handle success
           console.log(response.data.meals);
           setMealList(response.data.meals);
+          setLoading(false);
         });
     }, 500);
     return () => {
@@ -28,7 +34,14 @@ function Home() {
     <div id="home">
       <Navbar />
       <Searchbox searchMeal={{ searchMeal, setSearchMeal }} />
-      <Meal mealList={mealList} />
+      <h1 className="meal-title">Meal</h1>
+      {loading ? (
+        <div>
+          <Loading />
+        </div>
+      ) : (
+        <Meal mealList={mealList} />
+      )}
     </div>
   );
 }
