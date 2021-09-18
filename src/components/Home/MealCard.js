b169946aea18a65e64a52777dc7192a079d64d5e
christycrt <christycrt@gmail.com>
2021-09-18 18:38:53 +0700

    [update] meal card

--- src/components/Home/MealCard.js
+++ src/components/Home/MealCard.js
@@ -1,16 +1,12 @@
 function MealCard({ item }) {
   return (
     <article id="meal-card">
-      <img
-        className="meal-img"
-        src={item.strMealThumb}
-        alt="meal"
-      />
+      <img className="meal-img" src={item.strMealThumb} alt="meal" />
       <div className="meal-detail">
         <h3>{item.strMeal}</h3>
         <h4>{item.strCategory}</h4>
         <p className="text-meal-area">{item.strArea}</p>
-        <p>#{item.strTags}</p>
+        {item.strTags ? <p>#{item.strTags} </p> : ""}
       </div>
     </article>
   );
