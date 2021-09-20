5dc98e957a72d03cd5965e374ad06cfd00a075a5
christycrt <christycrt@gmail.com>
2021-09-20 16:40:45 +0700

    [update] hover on meal card and link to single meal page

--- src/components/Home/MealCard.js
+++ src/components/Home/MealCard.js
@@ -1,13 +1,17 @@
+import { Link } from "react-router-dom";
+
 function MealCard({ item }) {
   return (
     <article id="meal-card">
-      <img className="meal-img" src={item.strMealThumb} alt="meal" />
-      <div className="meal-detail">
-        <h3>{item.strMeal}</h3>
-        <h4>{item.strCategory}</h4>
-        <p className="text-meal-area">{item.strArea}</p>
-        {item.strTags ? <p>#{item.strTags} </p> : ""}
-      </div>
+      <Link to={`/meal/${item.idMeal}`}>
+        <img className="meal-img" src={item.strMealThumb} alt="meal" />
+        <div className="meal-detail">
+          <h3>{item.strMeal}</h3>
+          <h4>{item.strCategory}</h4>
+          <p className="text-meal-area">{item.strArea}</p>
+          {item.strTags ? <p>#{item.strTags} </p> : ""}
+        </div>
+      </Link>
     </article>
   );
 }
