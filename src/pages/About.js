62cd94ebfd024b2418635d1cb250e689c3230835
christycrt <christycrt@gmail.com>
2021-09-20 16:45:37 +0700

    [update] about page

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import "../styles/pages/About.scss";
+
+function About() {
+  return (
+    <section id="about">
+      <h1 className="about-title">About Us</h1>
+      <p className="text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        repudiandae architecto qui adipisci in officiis, aperiam sequi atque
+        perferendis eos, autem maiores nisi saepe quisquam hic odio consectetur
+        nobis veritatis quasi explicabo obcaecati doloremque? Placeat ratione
+        hic aspernatur error blanditiis?
+      </p>
+    </section>
+  );
+}
+
+export default About;
